db5cf54cea91aa01574a1494d3428554b07b01b5
Michaelchinex <okohagchidinma@gmail.com>
2022-07-19 18:43:08 -0700

    fixed

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
-    for count in range ( 0 x)
+    for count in range (0, x):
     try:
-        print(" {:d}". format(my_list[i], end = " ")
+        print(" {:d}". format(my_list[i], end="")
                 count += 1
                 except:
-                return the reall number
+                return the real number
                 print()
                 return count
