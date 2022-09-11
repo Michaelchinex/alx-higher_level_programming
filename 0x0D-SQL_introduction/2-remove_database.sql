114630839abf22e92c9c2b90d32ad5c1e5598a68
Michaelchinex <okohagchidinma@gmail.com>
2022-09-11 02:49:54 -0700

    Drop task

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,3 +1,3 @@
 -- a scripts that deletes databse hbtn_0c_0 in your MySQL server
 -- deletes database even if it doesn't exist
-DELETE DATABASE IF EXISTS hbtn_0c_0;
+DROP DATABASE IF EXISTS hbtn_0c_0;
