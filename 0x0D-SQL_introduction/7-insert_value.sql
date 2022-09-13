258c37a44818594456f175dd90e510f2127bde01
Michaelchinex <okohagchidinma@gmail.com>
2022-09-13 10:55:43 -0700

    hot fix

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 -- a script that inserts a new row in the table first_table
 -- a script that inserts a new row in the table 
-INSERT INTO 'first_table' ('id', 'name' value ('89', 'Holberton Schoo');
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, `Holberton School`);
